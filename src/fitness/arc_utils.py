3c52c5e1c7d8ae50a492b78b17559966c6a62306
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2020-03-25 10:58:33 +0100

    added arc_utils skeleton

---
+++ src/fitness/arc_utils.py
@@ -0,0 +1,20 @@
+def mode(m, sel):
+    pass
+
+def first(m, sel):
+    pass
+
+def rectangle(point1, point2):
+    pass
+
+def neighborhood(point, radius):
+    pass
+
+def selection_equals(m, sel1, sel2):
+    pass
+
+def any_hidden(m, sel):
+    pass
+
+def all_hidden(m, sel):
+    pass
