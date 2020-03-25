0a2e1f0cbef60ae663c6035fcd9aada001e43874
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2020-03-25 11:11:06 +0100

    added selection_equals, any_hidden, all_hidden, mode and first

--- src/fitness/arc_utils.py
+++ src/fitness/arc_utils.py
@@ -1,8 +1,16 @@
+import numpy as np
+
 def mode(m, sel):
-    pass
+    color_dist = np.zeros(10)
+    for x, y in sel:
+        color = m[x, y]
+        if color is not None:
+            color_dist[color] += 1
+
+    return np.argmax(color_dist) 
 
 def first(m, sel):
-    pass
+    return m[sel[0][0], sel[0][1]]
 
 def rectangle(point1, point2):
     pass
@@ -11,10 +19,20 @@ def neighborhood(point, radius):
     pass
 
 def selection_equals(m, sel1, sel2):
-    pass
+    c_1 = np.array(list(map(lambda x, y: m[x, y], sel1)))
+    c_2 = np.array(list(map(lambda x, y: m[x, y], sel2)))
+    return np.all(c_1 == c_2)
 
 def any_hidden(m, sel):
-    pass
+    for x, y in sel:
+        if m[x, y] is None:
+            return True
+
+    return False
 
 def all_hidden(m, sel):
-    pass
+    for x, y in sel:
+        if m[x, y] is not None:
+            return False
+
+    return True
